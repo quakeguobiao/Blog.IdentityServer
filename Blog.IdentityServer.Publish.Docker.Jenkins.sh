6fbd1db5079a0baabe77fd718bd6484eb8705210
quakeguobiao <quakeguobiao@gmail.com>
2022-05-08 12:31:05 +0800

    定制jenkins计划

---
+++ Blog.IdentityServer.Publish.Docker.Jenkins.sh
@@ -0,0 +1,22 @@
+﻿# 停止容器
+#docker stop idscontainer
+# 删除容器
+#docker rm idscontainer
+# 删除镜像
+#docker rmi laozhangisphi/idsimg
+# 切换目录
+#cd /home/Blog.IdentityServer
+# 发布项目
+chmod +x Blog.Core.Publish.Linux.sh
+./Blog.IdentityServer.Publish.Linux.sh
+# 进入目录
+cd .PublishFiles/
+# 编译镜像
+docker build -t quakeguobiao/idsimg .
+
+
+docker push quakeguobiao/idsimg:latest
+# 生成容器
+docker run --name=idscontainer -d -v /home/identityserver/appsettings.json:/app/appsettings.json -v /home/identityserver/Log/:/app/Log -v /etc/localtime:/etc/localtime -it -p 5004:5004 quakeguobiao/idsimg
+# 启动容器
+#docker start idscontainer
--- Blog.IdentityServer.Publish.Linux.sh
+++ Blog.IdentityServer.Publish.Linux.sh
@@ -1,6 +1,5 @@
-git pull;
 rm -rf .PublishFiles;
 dotnet build;
-dotnet publish -o /home/Blog.IdentityServer/Blog.IdentityServer/bin/Debug/netcoreapp;
-cp -r /home/Blog.IdentityServer/Blog.IdentityServer/bin/Debug/netcoreapp .PublishFiles;
+dotnet publish -o Blog.IdentityServer/bin/Debug/netcoreapp;
+cp -r Blog.IdentityServer/bin/Debug/netcoreapp .PublishFiles;
 echo "Successfully!!!! ^ please see the file .PublishFiles";
